0336120b41a6f2979d191d0a723bc2fa0015f4c2
puffin94 <65535216+puffin94@users.noreply.github.com>
2022-12-06 21:04:22 +0000

    removed unused imports

--- src/main/java/com/puffin94/day6/Day6.java
+++ src/main/java/com/puffin94/day6/Day6.java
@@ -3,10 +3,7 @@ package main.java.com.puffin94.day6;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Day6 {
 
